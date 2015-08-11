0c2cb75034eff6a4451964ed17a573c14a439523
MaLeVoLenT1 <malevolent@creative-onslaught.com>
2015-08-11 19:01:42 -0400

    Order Controller and blade files updated and finished. Edit, Delete, create, update, and show are all finished.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,10 +10,11 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-Route::get('/', 'WelcomeController@index');
+Route::get('/', 'HomeController@index');
 Route::get('home', 'HomeController@index');
 
 Route::resource('orders', 'OrdersController');
+Route::resource('instruments', 'InstrumentsController');
 
 Route::get('users/index', 'UsersController@index');
 
