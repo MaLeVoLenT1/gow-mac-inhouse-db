59a6cd2a4c0f842087a92ad81efbf1dce9cd54af
MaLeVoLenT1 <malevolent@creative-onslaught.com>
2015-09-18 18:17:15 -0400

    Form work.

--- public/javascript/instruments.js
+++ public/javascript/instruments.js
@@ -4,9 +4,6 @@ $(function () {
         var num = $('.clonedInput').length, // how many "duplicatable" input fields we currently have
             newNum = new Number(num + 1), // the numeric ID of the new input field being added
             newElem = $('#testingDiv' + num).clone().attr('id', 'testingDiv' + newNum).fadeIn('slow'); // create the new element via clone(), and manipulate it's ID using newNum value
-        // manipulate the name/id values of the input inside the new element
-
-
 
         // insert the new element after the last "duplicatable" input field
         $('#testingDiv' + num).after(newElem);
